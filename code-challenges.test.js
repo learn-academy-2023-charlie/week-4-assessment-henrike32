d664a9f7ba09628b8f66861a0bda50e8dee843d4
henrike32 <henrique.lambertucci@gmail.com>
2023-05-16 11:30:57 -0400

    did the second exercise, 1 to go, will try to finish at lunch time

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -3,15 +3,11 @@
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
-
 // Use test driven development to complete the following questions
 // Add appropriate dependencies: $ yarn add jest
-
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
-
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
-
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
@@ -22,7 +18,6 @@
 //   });
 // })
 //  ReferenceError: randomColors is not defined
-
 // b) Create the function that makes the test pass.
 
 // pseudocode ->
@@ -48,55 +43,57 @@ const colors1 = ["purple", "blue", "green", "yellow", "pink"];
 //Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]✅
 const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"];
 //Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]✅
-// console.log(rColors(colors1));
-// console.log(rColors(colors2));
-
+console.log(rColors(colors1));
+console.log(rColors(colors2));
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-// describe(votesCount, () => {
-//   const votes1 = { upVotes: 13, downVotes: 2 }
-//   // Expected output: 11
-//   const votes2 = { upVotes: 2, downVotes: 33 }
-//   // Expected output: -31
-//   it("returns the voting end tally", () => {
-//     expect(votes1()).toEqual(11)
-//     expect(votes2()).toEqual(-31)
-//   })
-// })
-
+// describe('votesCount', () => {
+//   it('returns the voting end tally', () => {
+  const votes1 = { upVotes: 13, downVotes: 2 };
+// Expected output: 11
+const votes2 = { upVotes: 2, downVotes: 33 };
+// Expected output: -31
+//     expect(votesCount(votes1)).toEqual(11);
+//     expect(votesCount(votes2)).toEqual(-31);
+//   });
+// });
 // ReferenceError: votesCount is not defined
-
-
 // b) Create the function that makes the test pass.
 
 // pseudocode ->
 // input -> objects
-// output -> sum of votes
+// output -> a number representing the subtraction between upVotes - downVotes
 // process ->
+// create a function named votesCount which will take in votes
+// create a variable with the keys you have in your object
+// return the subtraction between the key:values.
+const votesCount = (votes) => {
+  const { upVotes, downVotes } = votes;
+  return upVotes - downVotes;
+};
 
+console.log(votesCount(votes1));
+console.log(votesCount(votes2));
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
-
 // a) Create a test with an expect statement using the variables provided.
-// describe(dataTypeZ, () => {
-//   it("returns thed ata types", () => {
-//     const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
-// // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
-//     const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
-//     expect(dataTypesArray1()).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
-//     expect(dataTypesArray2()).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
-//   })
-// })
+
+describe(dataTypeZ, () => {
+  it("returns thed ata types", () => {
+    const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
+// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+    const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
+// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+    expect(dataTypesArray1()).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+    expect(dataTypesArray2()).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+  })
+})
 // ReferenceError: dataTypez is not defined
-
-
-
 // b) Create the function that makes the test pass.
 
 // pseudocode ->
 // input -> takes in two arrays
 // output ->  one array with no duplicate values
 // process ->
-
+// 
